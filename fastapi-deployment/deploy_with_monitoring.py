74b1f404c11d01d5c0835790e4c31a9910bec2ab
igvakhlamov <igvakhlamov@gmail.com>
2023-04-15 12:44:06 +0300

    the best monitoring

--- fastapi-deployment/deploy_with_monitoring.py
+++ fastapi-deployment/deploy_with_monitoring.py
@@ -1,46 +1,88 @@
-from typing import Callable
-
-from mlem.api import load_meta, serve
-from mlem.contrib.fastapi import FastAPIServer, Middlewares
-from mlem.contrib.prometheus import PrometheusFastAPIMiddleware
 from prometheus_fastapi_instrumentator.metrics import Info
-from prometheus_client import Counter
-from prometheus_fastapi_instrumentator import Instrumentator
+import numpy as np
+from PIL import Image
+from prometheus_client import Counter, Histogram, Gauge
 
 
-def http_requested_languages_total() -> Callable[[Info], None]:
-    METRIC = Counter(
-        "http_requested_languages_total",
-        "Number of times a certain language has been requested.",
-        labelnames=("langs",)
-    )
-
-    def instrumentation(info: Info) -> None:
-        langs = set()
-        lang_str = info.request.headers["Accept-Language"]
-        for element in lang_str.split(","):
-            element = element.split(";")[0].strip().lower()
-            langs.add(element)
-        for language in langs:
-            METRIC.labels(language).inc()
-
-    return instrumentation
-
-instrumentator = Instrumentator()
-instrumentator.add(http_requested_languages_total())
-
-model = load_meta("price")
-
-server = FastAPIServer(
-    standardize=True,
-    middlewares=Middlewares(
-        __root__=[PrometheusFastAPIMiddleware(instrumentator=instrumentator)]
-    ),
+RANDOM_VALUE = Gauge(
+    "random_value",
+    "Random value that is set to show monitoring works"
 )
 
-deploy(
-    "app-fastapi.mlem",
-    model=model,
-    server=server,
-    app_name="art-expert-fastapi",
+def random_value(info: Info) -> None:
+    RANDOM_VALUE.set(np.random.random())
+
+RANDOM_VALUE_NORM = Gauge(
+    "random_value_norm",
+    "Random value that is set to show monitoring works"
 )
+
+def random_value_norm(info: Info) -> None:
+    RANDOM_VALUE_NORM.set(np.random.normal())
+
+
+METRIC_EXAMPLE = Counter(
+    "http_requested_languages_total",
+    "Number of times a certain language has been requested.",
+    labelnames=("langs",)
+)
+
+METRIC_EXAMPLE_AGENT = Counter(
+    "http_requested_agents_total",
+    "Number of times a certain agent requested metrics.",
+    labelnames=("agents",)
+)
+
+
+def http_requested_languages_total(info: Info) -> None:
+    langs = set()
+    try:
+        list = [c for c in info.request.headers.keys() if "Accept-Language".lower() == c.lower()]
+        if len(list) > 0:
+            lang_str = info.request.headers[list[0]]
+            for element in lang_str.split(","):
+                element = element.split(";")[0].strip().lower()
+                langs.add(element)
+            for language in langs:
+                METRIC_EXAMPLE.labels(language).inc()
+            print(METRIC_EXAMPLE)
+        else:
+            METRIC_EXAMPLE.labels('nan').inc()
+    except:
+        METRIC_EXAMPLE.labels('nan').inc()
+
+
+def http_users_agent_total(info: Info) -> None:
+    agents = set()
+    try:
+        list = [c for c in info.request.headers.keys() if "user-agent".lower() == c.lower()]
+        if len(list) > 0 :
+            agent = info.request.headers[list[0]].split('/')[0]
+            METRIC_EXAMPLE.labels(agent).inc()
+            for agentt in agents:
+                METRIC_EXAMPLE_AGENT.labels(agentt).inc()
+            print(METRIC_EXAMPLE_AGENT)
+        else:
+            METRIC_EXAMPLE_AGENT.labels('nan').inc()
+    except:
+        METRIC_EXAMPLE_AGENT.labels('nan').inc()
+
+
+MEAN_PIXEL_VALUE = Histogram(
+    'image_middle_pixel_value',
+    'Mean pixel value of the image',
+    buckets=(0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275),
+)
+
+
+async def image_middle_pixel_value(info: Info) -> None:
+    # this would have been a way to load the image
+    # and calculate some metrics, but it doesn't work
+    # (limitations of `prometheus_fastapi_instrumentator`)
+    # keeping the code piece as an example though
+    # if info.request.url.path == '/predict':
+    #     form = await info.request.form()
+    #     im = Image.open(form['file'].file)
+    #     mean_value = np.array(im).mean()
+    #     MEAN_PIXEL_VALUE.observe(mean_value)
+    pass
--- fastapi-deployment/lang_metric.py
+++ fastapi-deployment/lang_metric.py
@@ -3,6 +3,15 @@ import numpy as np
 from PIL import Image
 from prometheus_client import Counter, Histogram, Gauge
 
+RANDOM_VALUE_NORM = Gauge(
+    "random_value_norm",
+    "Random value norm that is set to show monitoring works"
+)
+
+
+def random_value_norm(info: Info) -> None:
+    RANDOM_VALUE_NORM.set(np.random.normal())
+
 
 RANDOM_VALUE = Gauge(
     "random_value",
@@ -12,23 +21,55 @@ RANDOM_VALUE = Gauge(
 def random_value(info: Info) -> None:
     RANDOM_VALUE.set(np.random.random())
 
-
-
 METRIC_EXAMPLE = Counter(
     "http_requested_languages_total",
     "Number of times a certain language has been requested.",
     labelnames=("langs",)
 )
 
+METRIC_EXAMPLE_AGENT = Counter(
+    "http_requested_agents_total",
+    "Number of times a certain agent requested metrics.",
+    labelnames=("agents",)
+)
+
 
 def http_requested_languages_total(info: Info) -> None:
     langs = set()
-    lang_str = info.request.headers["Accept-Language"]
-    for element in lang_str.split(","):
-        element = element.split(";")[0].strip().lower()
-        langs.add(element)
-    for language in langs:
-        METRIC_EXAMPLE.labels(language).inc()
+    try:
+        list = [c for c in info.request.headers.keys() if "Accept-Language".lower() == c.lower()]
+        if len(list) > 0:
+            lang_str = info.request.headers[list[0]]
+            for element in lang_str.split(","):
+                element = element.split(";")[0].strip().lower()
+                langs.add(element)
+            for language in langs:
+                METRIC_EXAMPLE.labels(language).inc()
+            print(METRIC_EXAMPLE)
+        else:
+            METRIC_EXAMPLE.labels('nan').inc()
+    except:
+        METRIC_EXAMPLE.labels('nan').inc()
+
+
+def http_users_agent_total(info: Info) -> None:
+    try:
+        list = [c for c in info.request.headers.keys() if "user-agent".lower() == c.lower()]
+        if len(list) > 0 :
+            agent_ = info.request.headers[list[0]].split('/')[0]
+            if agent_ in ('TelegramBot (like TwitterBot)', 'python-requests'):
+                agent = 'Telegram'
+            elif agent_ in ('Mozilla'):
+                agent = 'Web'
+            elif agent_ in ('Vector'):
+                agent = 'prometheus'
+            else:
+                agent = agent_
+            METRIC_EXAMPLE_AGENT.labels(agent).inc()
+        else:
+            METRIC_EXAMPLE_AGENT.labels('nan').inc()
+    except:
+        METRIC_EXAMPLE_AGENT.labels('nan').inc()
 
 
 MEAN_PIXEL_VALUE = Histogram(
@@ -43,7 +84,9 @@ async def image_middle_pixel_value(info: Info) -> None:
     # and calculate some metrics, but it doesn't work
     # (limitations of `prometheus_fastapi_instrumentator`)
     # keeping the code piece as an example though
-    form = await info.request.form()
-    im = Image.open(form['file'].file)
-    mean_value = np.array(im).mean()
-    MEAN_PIXEL_VALUE.observe(mean_value)
+    # if info.request.url.path == '/predict':
+    #     form = await info.request.form()
+    #     im = Image.open(form['file'].file)
+    #     mean_value = np.array(im).mean()
+    #     MEAN_PIXEL_VALUE.observe(mean_value)
+    pass
